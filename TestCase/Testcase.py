0de6f29254a85bf9733c89d3e7ccfa3c9d429b04
WIN <vishalmanojvs@gmail.com>
2024-09-21 10:13:12 +0530

    Framework

--- TestCase/Testcase.py
+++ TestCase/Testcase.py
@@ -1,7 +1,31 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-import time
-import requests
+import pytest
+from selenium.webdriver.support.wait import WebDriverWait
+from Configfiles.config import TestData
+from Pages.page import LoginPage
+from Utilies.Logging import LogGen
+from Utilies.utilies import Utils
+from TestCase.conftest import setup
+logger = LogGen.loggen()
+
+
+@pytest.mark.usefixtures("setup")
+class TestLogin:
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+    def test_valid_login(self):
+        logger.info("Starting test_valid_login")
+        self.driver.get(TestData.BASE_URL)
+        login_page = LoginPage(self.driver)
+        login_page.enter_phone_number(TestData.PHONE_NUMBER)
+        login_page.click_sign_in()
+
+        # Example for OTP
+        otp = "123456"  # Replace with actual OTP fetching logic
+        login_page.enter_otp(otp)
+        login_page.click_verify()
+
+        assert login_page.is_visible(LoginPage.VERIFY_BUTTON), "Login failed"
+        Utils.take_screenshot(self.driver, "login_success")
